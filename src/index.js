ef2683565a49e59987d7c6c200ffc5fd1d2b481d
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-05-26 10:06:49 -0500

    :construction: 13 - Class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const messages = ["Jozek", "Nicolas", "Oscar", "Ana", "Laura", "Andres"];
+const messages = ["Jozek", "Nicolas", "Oscar", "Ana", "Laura", "Andres", "Mar", "Paulina", "John", "Johns", "Hernesto"];
 
 const randomMsg = () => {
   const message = messages[Math.floor(Math.random() * messages.length)];
